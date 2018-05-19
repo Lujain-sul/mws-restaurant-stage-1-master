90d3876ca0d3b434450ed0695bcfbd544c4ec6d1
Lujain-sul <37689271+Lujain-sul@users.noreply.github.com>
2018-05-19 08:53:17 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Restaurant Reviews Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+1. About the project
+
+The project transforms static design for restaurants reviews into responsive and accessible one. It also implements a service worker to offer offline experience.
+
+2. Prerequisite
+
+Python software should be installed [website](https://www.python.org/).
+
+3. How to run the project
+
+- If you have Python 2.x, spin up the server with `python -m SimpleHTTPServer 8000` For Python 3.x, you can use `python3 -m http.server 8000`
+- With your server running, visit the site: `http://localhost:8000`
+
+
+## Contributing
+
+This project is built in the fulfillment of Udacity Front End Nano Degree requirement, pull requests will not be merged to this project.
---
+++ offline_mode.js
@@ -0,0 +1,49 @@
+/* Service worker guide from https://codelabs.developers.google.com/codelabs/offline/#0
+*  Copyright 2015 Google Inc. All rights reserved.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License
+*
+*/
+const version = "0.0.1";
+const cacheName = `restaurant-reviews-${version}`;
+self.addEventListener('install', e => {
+  const timeStamp = Date.now();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html?timestamp=${timeStamp}`,
+        `/restaurant.html?timestamp=${timeStamp}`,
+        `/css/styles.css?timestamp=${timeStamp}`,
+        `/js/main.js?timestamp=${timeStamp}`,
+        `/js/dbhelper.js?timestamp=${timeStamp}`,
+        `/js/restaurant_info.js?timestamp=${timeStamp}`
+      ])
+      .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+    .then(cache => cache.match(event.request, {ignoreSearch: true}))
+    .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
